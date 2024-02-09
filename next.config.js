54bb77dc27bd3c9908bbda6896c810e4e9a8d0c0
杨飞 <yangfei86@vip.qq.com>
2024-02-09 11:30:34 +0800

    google seo 支持

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const { securityHeaders } = require('./project.config');
+const nextBuildId = require('next-build-id');
 // const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
@@ -20,6 +21,8 @@ const nextConfig = {
       },
     ];
   },
+  // Small package to generate a consistent, git-based build id for your Next.js app when running next build on each server in a multi-server deployment.
+  generateBuildId: () => nextBuildId({ dir: __dirname }),
   webpack: (config, options) => {
     config.module.rules.push({
       test: /\.mdx?$/,
