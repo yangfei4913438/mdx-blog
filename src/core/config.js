e675699e5434f5863c4439f51bfc565b13e9827d
杨飞 <yangfei86@vip.qq.com>
2024-02-28 08:49:20 +0800

    优化文章的目录结构设计

--- src/core/config.js
+++ src/core/config.js
@@ -1,22 +1,28 @@
 // 目录排序配置
 const dirOrderConfig = {
   // 这里的 frontend 就是一级目录的名称，下同
-  frontend: {
+  React: {
     // 这里的 index 就是这个一级目录的排序，下同
     index: 0,
     // 这里的 children 表示下级目录，需要根据这个属性查询下级目录，下同
     children: {
-      react: 0, // 下级目录的名称和排序，下同
-      rn: 1,
-      js: 2,
-      css: 3,
-      dom: 4,
-      test: 5,
+      优化: 0, // 下级目录的名称和排序，下同
+      状态管理: 1,
+      hooks: 2,
+      工程化: 3,
+      拖拽: 4,
+      测试: 5,
       antd: 6,
     },
   },
-  linux: {
+  ReactNative: {
     index: 1,
+    children: {
+      基础: 0,
+    },
+  },
+  linux: {
+    index: 2,
     children: {
       server: 0,
       docker: 1,
@@ -24,10 +30,18 @@ const dirOrderConfig = {
       github: 3,
     },
   },
-  algorithm: {
-    index: 2,
+  算法: {
+    index: 3,
     children: {
-      quick_sort: 0,
+      快速排序: 0,
+    },
+  },
+  前端基础: {
+    index: 4,
+    children: {
+      js: 0,
+      css: 1,
+      dom: 2,
     },
   },
 };
