259a0649c88c251af417cbcdf720556a964b79de
杨飞 <yangfei86@vip.qq.com>
2024-03-09 14:22:04 +0800

    优化 404 页面

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,23 +1,23 @@
 import { useRouter } from 'next/router';
-import { useCallback, useEffect } from 'react';
+import { useEffect } from 'react';
 import SeoLink from '@/components/link';
+import useNextLink from '@/hooks/useNextLink';
 
 const Custom404 = () => {
   const router = useRouter();
-
-  const handleGoHome = useCallback(() => {
-    router.push({ pathname: '/' });
-  }, [router]);
+  const { handleLink } = useNextLink();
 
   useEffect(() => {
-    setTimeout(handleGoHome, 3000);
-  }, [handleGoHome, router]);
+    setTimeout(() => {
+      router.push('/');
+    }, 3000);
+  }, [router]);
 
   return (
     <div className='flex h-screen w-screen flex-col items-center justify-center bg-gray-100'>
       <h1 className='text-3xl text-gray-9'>很抱歉! 您访问的页面不存在!</h1>
       <p className='mb-4 mt-8 text-base text-md text-gray-600'>正在为您自动跳转到首页，请稍后...</p>
-      <SeoLink href='/' className='text-red-500' onClick={handleGoHome}>
+      <SeoLink href='/' className='text-red-500' onClick={handleLink('/')}>
         点击立刻跳转
       </SeoLink>
     </div>
